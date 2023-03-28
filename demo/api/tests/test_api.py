6b3da200d8f3161a89a82e0b16ec048b379b75de
Nicolas Neubauer <nic@planet.com>
2023-03-28 22:21:17 +0200

    add Planet to test suite

--- demo/api/tests/test_api.py
+++ demo/api/tests/test_api.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from ..main import app
+from ..backends import BACKENDS
 
 
 client = TestClient(app)
@@ -40,10 +41,21 @@ def test_post_to_pineapple_with_pineapple_body_and_header(backend):
     assert "features" in response.json()
 
 
+def test_post_to_planet():
+    response = client.post(
+        "/pineapple",
+        headers={"Backend": "planet"}, # , "Authorization": f"Bearer {API_TOKEN}"},
+        json={"bbox": [0,0,1,1]},
+    )
+    assert response.status_code == 200
+    assert "bbox" in response.json()
+    assert "features" in response.json()
+
+
 def test_post_to_pineapple_with_bad_backend_raises():
     response = client.post(
         "/pineapple",
         headers={"Backend": "foo"},
     )
     assert response.status_code == 404
-    assert response.json()["detail"] == "Backend 'foo' not in options: ['fake', 'sentinel']"
+    assert response.json()["detail"] == f"Backend 'foo' not in options: {list(BACKENDS.keys())}"
