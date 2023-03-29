b2a12866f5d051d01d8cc4e5e481809142e64fd9
Julia Signell <jsignell@gmail.com>
2023-03-29 11:34:58 -0400

    Different input and output types

--- demo/api/api_types.py
+++ demo/api/api_types.py
@@ -1,8 +1,6 @@
-from typing import Any, Optional, Union
-from geojson_pydantic.features import Feature, FeatureCollection
-from pydantic import constr, root_validator, BaseModel
-from stac_pydantic.item import ItemProperties
-from stac_pydantic.shared import BBox
+from typing import Any, Optional, Union, Dict
+from geojson_pydantic.features import Feature
+from pydantic import BaseModel, Field
 from pydantic.datetime_parse import parse_datetime
 from geojson_pydantic.geometries import (
     GeometryCollection,
@@ -13,20 +11,40 @@ from geojson_pydantic.geometries import (
     Point,
     Polygon,
 )
+from stac_pydantic.shared import DATETIME_RFC339
 
-from datetime import datetime as pydatetime
+from datetime import datetime as Datetime
+
+Geometry = Union[
+    Point,
+    MultiPoint,
+    LineString,
+    MultiLineString,
+    Polygon,
+    MultiPolygon,
+    GeometryCollection,
+]
+
+class OpportunityProperties(BaseModel):
+    """
+    https://github.com/radiantearth/stac-spec/blob/v1.0.0/item-spec/common-metadata.md#date-and-time-range
+    """
+
+    title: Optional[str] = Field(None, alias="title")
+    description: Optional[str] = Field(None, alias="description")
+    start_datetime: Optional[Datetime] = Field(None, alias="start_datetime")
+    end_datetime: Optional[Datetime] = Field(None, alias="end_datetime")
+    product_id: Optional[str] = Field(None, alias="product_id")
+    constraints: Optional[Dict[str, Any]] = Field(None, alias="constraints")
+
+    class Config:
+        json_encoders = {Datetime: lambda v: v.strftime(DATETIME_RFC339)}
+
 
 # Copied and modified from stack_pydantic.item.Item
-class Item(Feature[Any, ItemProperties]):
-
-    id: constr(min_length=1)  # type: ignore
-
-    # Should there be a stac_version?
-    # stac_version: constr(min_length=1) = Field(STAC_VERSION, const=True)
-
-    properties: ItemProperties
-    # TODO should we have links
-    # links: Links
+class Opportunity(Feature[Geometry, OpportunityProperties]):
+    id: Optional[str]
+    properties: OpportunityProperties
 
     def to_dict(self, **kwargs: Any):
         return self.dict(by_alias=True, exclude_unset=True, **kwargs)
@@ -34,62 +52,24 @@ class Item(Feature[Any, ItemProperties]):
     def to_json(self, **kwargs: Any):
         return self.json(by_alias=True, exclude_unset=True, **kwargs)
 
-    @root_validator(pre=True)
-    def validate_bbox(cls, values: dict[str, Any]):
-        if values.get("geometry") and values.get("bbox") is None:
-            raise ValueError("bbox is required if geometry is not null")
-        return values
-
-# Copied and modified from stack_pydantic.item.ItemCollection
-class ItemCollection(FeatureCollection[Any, Any]):
-    """
-    https://github.com/radiantearth/stac-spec/blob/v1.0.0/item-spec/itemcollection-spec.md
-    """
-
-    # Should there be a stac_version?
-    # stac_version: constr(min_length=1) = Field(STAC_VERSION, const=True)
-
-    features: list[Item]
-
-    # stac_extensions: Optional[List[AnyUrl]]
-
-    # TODO should we have links
-    # links: Links
-    # context: Optional[ContextExtension] = None
-
-    def to_dict(self, **kwargs: Any):
-        return self.dict(by_alias=True, exclude_unset=True, **kwargs)
-
-    def to_json(self, **kwargs: Any):
-        return self.json(by_alias=True, exclude_unset=True, **kwargs)
 
 
 # Copied and modified from stack_pydantic.api.search.Search
-class Search(BaseModel):
+class Search():
+    geometry: Optional[Geometry]
 
-    bbox: Optional[BBox]
-    intersects: Optional[
-        Union[
-            Point,
-            MultiPoint,
-            LineString,
-            MultiLineString,
-            Polygon,
-            MultiPolygon,
-            GeometryCollection,
-        ]
-    ]
     # Slash separated date time range
     datetime: str
-    limit: int = 10
+    product_id: str
+    constraints: Optional[Dict[str, Any]]
 
     # TODO need to ask if this is exactly like stac with .., /, single datetime etc.
     @property
-    def start_date(self) -> pydatetime:
+    def start_date(self) -> Datetime:
         values = self.datetime.split("/")
         return parse_datetime(values[0])
 
     @property
-    def end_date(self) -> pydatetime:
+    def end_date(self) -> Datetime:
         values = self.datetime.split("/")
         return parse_datetime(values[1])
