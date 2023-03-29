35380d71c68d7356d4efc066ec66a16d7de6b780
Jason Gilman <jason@element84.com>
2023-03-29 08:34:19 -0400

    Intermediate commit

---
+++ demo/api/api_types.py
@@ -0,0 +1,95 @@
+from typing import Any, Optional, Union
+from geojson_pydantic.features import Feature, FeatureCollection
+from pydantic import constr, root_validator, BaseModel
+from stac_pydantic.item import ItemProperties
+from stac_pydantic.links import Links
+from stac_pydantic.api.extensions.context import ContextExtension
+from stac_pydantic.shared import BBox
+from pydantic.datetime_parse import parse_datetime
+from geojson_pydantic.geometries import (
+    GeometryCollection,
+    LineString,
+    MultiLineString,
+    MultiPoint,
+    MultiPolygon,
+    Point,
+    Polygon,
+)
+
+from datetime import datetime as pydatetime
+
+# Copied and modified from stack_pydantic.item.Item
+class Item(Feature[Any, ItemProperties]):
+
+    id: constr(min_length=1)  # type: ignore
+
+    # Should there be a stac_version?
+    # stac_version: constr(min_length=1) = Field(STAC_VERSION, const=True)
+
+    properties: ItemProperties
+    links: Links
+
+    def to_dict(self, **kwargs: Any):
+        return self.dict(by_alias=True, exclude_unset=True, **kwargs)
+
+    def to_json(self, **kwargs: Any):
+        return self.json(by_alias=True, exclude_unset=True, **kwargs)
+
+    @root_validator(pre=True)
+    def validate_bbox(cls, values: dict[str, Any]):
+        if values.get("geometry") and values.get("bbox") is None:
+            raise ValueError("bbox is required if geometry is not null")
+        return values
+
+# Copied and modified from stack_pydantic.item.ItemCollection
+class ItemCollection(FeatureCollection[Any, Any]):
+    """
+    https://github.com/radiantearth/stac-spec/blob/v1.0.0/item-spec/itemcollection-spec.md
+    """
+
+    # Should there be a stac_version?
+    # stac_version: constr(min_length=1) = Field(STAC_VERSION, const=True)
+
+    features: list[Item]
+
+    # stac_extensions: Optional[List[AnyUrl]]
+
+    links: Links
+    context: Optional[ContextExtension]
+
+    def to_dict(self, **kwargs: Any):
+        return self.dict(by_alias=True, exclude_unset=True, **kwargs)
+
+    def to_json(self, **kwargs: Any):
+        return self.json(by_alias=True, exclude_unset=True, **kwargs)
+
+
+# Copied and modified from stack_pydantic.api.search.Search
+class Search(BaseModel):
+
+    bbox: Optional[BBox]
+    intersects: Optional[
+        Union[
+            Point,
+            MultiPoint,
+            LineString,
+            MultiLineString,
+            Polygon,
+            MultiPolygon,
+            GeometryCollection,
+        ]
+    ]
+    # Slash separated date time range
+    datetime: str
+    limit: int = 10
+
+    # TODO need to ask if this is exactly like stac with .., /, single datetime etc.
+    @property
+    def start_date(self) -> pydatetime:
+        values = self.datetime.split("/")
+        return parse_datetime(values[0])
+
+    @property
+    def end_date(self) -> pydatetime:
+        values = self.datetime.split("/")
+        return parse_datetime(values[1])
--- demo/api/backends/sentinel_backend.py
+++ demo/api/backends/sentinel_backend.py
@@ -1,8 +1,61 @@
+import datetime
+import re
+from datetime import timedelta
+from typing import Any, Union
 
+import pystac
+from api.api_types import Item, ItemCollection, Search
 from pystac_client.client import Client
-from stac_pydantic import ItemCollection
-from stac_pydantic.api.search import Search
+from stac_pydantic.item import ItemProperties
+from stac_pydantic.links import Links
 
+DEFAULT_MAX_ITEMS = 10
+MAX_MAX_ITEMS = 100
+
+
+def get_max_items(search_request: Search) -> int:
+    max_items = DEFAULT_MAX_ITEMS
+    if search_request.limit:
+        max_items = search_request.limit
+    return min(max_items, MAX_MAX_ITEMS)
+
+# The api works by pretending the past is the future. It takes a users search request and searches for data in the
+# past. This is the amount of time in the past we search from a request.
+TIME_DELTA = timedelta(days=3*365)
+
+MaybeDate = Union[str, Any]
+
+
+def adjust_date_times(properties: dict[str, Any]) -> ItemProperties:
+    def adjust_date_time(value: MaybeDate) -> Any:
+        if isinstance(value, str) and re.match(r'\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d+.*', value):
+            try:
+                date = datetime.datetime.fromisoformat(value.replace('Z','+00:00'))
+                value = (date + TIME_DELTA).isoformat()
+            except Exception as e:
+                print(f'Could not parse {value} as a datetime')
+                raise e
+        return value
+
+    return ItemProperties(**{
+        k: adjust_date_time(v)
+        for k, v in properties.items()
+    })
+
+
+def stac_item_to_future_item(item: pystac.Item) -> Item:
+    return Item(
+        geometry=item.geometry,
+        properties=adjust_date_times(item.properties),
+        id=item.id,
+
+        # TODO implement bbox mapping
+        # bbox=item.bbox,
+
+        # TODO implement links mapping
+        # links=item.links
+        links=Links(__root__=[]),
+    )
 
 class SentinelBackend:
     async def find_future_items(
@@ -10,18 +63,44 @@ class SentinelBackend:
         search_request: Search,
         token: str,
     ) -> ItemCollection:
-        catalog = Client.open('https://earth-search.aws.element84.com/v1')
+        catalog = Client.open('https://earth-search.aws.element84.com/v1')  # type: ignore
 
         # https://earth-search.aws.element84.com/v1/collections/landsat-c2-l2/items
 
-        args = search_request.dict()
-        args['collections'] = 'landsat-c2-l2'
-        args['max_items'] = 10
+
+        max_items = get_max_items(search_request)
+
+        args: dict[str, Any] = {
+            'collections': 'landsat-c2-l2',
+            'max_items': max_items,
+            'limit': max_items,
+        }
+        if search_request.bbox:
+            args['bbox'] = search_request.bbox
+        if search_request.intersects:
+            args['intersects'] = search_request.intersects
+
+        if search_request.start_date and search_request.end_date:
+            args['datetime'] = [
+                search_request.start_date - TIME_DELTA,
+                search_request.end_date - TIME_DELTA,
+            ]
+        else:
+            raise Exception('A datetime range must be specified')
 
         search = catalog.search(**args)
-        item_coll =  search.item_collection()
+        item_coll = search.item_collection()
 
-        item_collection = ItemCollection(links=[], **item_coll.to_dict())
+        # Convert the STAC items from earth search into future items
+        items: list[Item] = [
+            stac_item_to_future_item(item)
+            for item in item_coll.items
+        ]
 
+        item_collection = ItemCollection(
+            links=Links(__root__=[]),
+            **item_coll.to_dict(),
+            features=items
+        )
 
         return item_collection
