d874d8cbf2b9ef98c0d679fdab065b892f0864b1
owcm <csm9105@gmail.com>
2023-03-29 16:00:06 -0400

    put back tests

--- demo/api/tests/test_api.py
+++ demo/api/tests/test_api.py
@@ -1,57 +1,68 @@
-import datetime
-import os
-
 import pytest
 from fastapi.testclient import TestClient
 
-from api.main import app
-from api.backends import BACKENDS
+from ..main import app
+from ..backends import BACKENDS
 
-import logging
-import json
-
-LOGGER = logging.getLogger(__name__)
 
 client = TestClient(app)
 
 VALID_SEARCH_BODY = {
     "datetime": "2025-01-01T00:00:00Z/2025-01-02T00:00:00Z",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [39.95, 75.16]
-    }
+    "bbox": [0,0,1,1]
 }
 
+def test_read_docs():
+    response = client.get("/docs")
+    assert response.status_code == 200
 
 
-@pytest.mark.parametrize("backend", ["blacksky"])
-def test_post_to_opportunities_with_opportunities_body_and_header_authenticated(backend: str):
-    token_name = f"{backend.upper()}_TOKEN"
+def test_post_to_pineapple_with_no_body():
+    response = client.post("/pineapple")
+    assert response.status_code == 422
 
-    if token_name not in os.environ:
-        # skip endpoint if token not provided
-        return
-    token = os.environ[token_name]
 
-    print("1")
-    start_time = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=1)
-    end_time = start_time + datetime.timedelta(days=3)
-
-    search_body_now = {
-        "datetime":  f"{start_time.isoformat()}/{end_time.isoformat()}",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [39.95, 75.16]
-        }
-    }
-
+def test_post_to_pineapple_with_pineapple_body():
     response = client.post(
-        "/opportunities",
-        headers={"Backend": backend, "Authorization": f"Bearer {token}"},
-        json=search_body_now,
+        "/pineapple",
+        json=VALID_SEARCH_BODY,
     )
     assert response.status_code == 200
+    assert "bbox" in response.json()
     assert "features" in response.json()
-    LOGGER.info(json.dumps(response.json(), indent=2))
 
 
+@pytest.mark.parametrize("backend", ["fake", "sentinel"])
+def test_post_to_pineapple_with_pineapple_body_and_header(backend: str):
+    response = client.post(
+        "/pineapple",
+        headers={"Backend": backend},
+        json=VALID_SEARCH_BODY,
+    )
+    assert response.status_code == 200
+    assert "bbox" in response.json()
+    assert "features" in response.json()
+
+# TODO currently failing
+# def test_post_to_planet():
+#     response = client.post(
+#         "/pineapple",
+#         headers={"Backend": "planet"}, # , "Authorization": f"Bearer {API_TOKEN}"},
+#         json={
+#             "bbox": [0,0,1,1],
+#             "datetime": "2023-03-30T17:20:13.061Z" # this should be start and end eventually
+#         },
+#     )
+#     assert response.status_code == 200
+#     assert "bbox" in response.json()
+#     assert "features" in response.json()
+
+
+def test_post_to_pineapple_with_bad_backend_raises():
+    response = client.post(
+        "/pineapple",
+        headers={"Backend": "foo"},
+        json=VALID_SEARCH_BODY,
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == f"Backend 'foo' not in options: {list(BACKENDS.keys())}"
